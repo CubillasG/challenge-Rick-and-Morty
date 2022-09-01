1baeb29dd2a4bc87503f280e98ebbf697d0673d9
gonza <gonzalo.cubillas15@gmail.com>
2022-09-01 19:16:07 -0300

    ajuste de modal

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,19 +1,23 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import imagen from "../assets/imagen.jpg";
 import "./Home.css";
 import ModalInfo from "./ModalInfo/ModalInfo";
 
-
 const Home = () => {
+  const[openModal, setOpenModal] = useState(false)
   return (
     <div className="container class_home">
       <div className="row">
         <div className="col-6">
           <div className="card">
-            <img src={imagen} alt="..." />
+           
+            <img src={imagen} alt="..." onClick={()=>{setOpenModal(true)}} />
+            
+           
           </div>
-          <ModalInfo/>
+          {openModal && <ModalInfo  closeModal={setOpenModal}/>}
         </div>
 
         <div className="col-6 claseHome_texto">
---
+++ src/Page/ModalInfo/ModalInfo.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import iconoClose from "../../assets/close.png";
+import "./ModalInfo.css";
+
+const ModalInfo = ({ closeModal }) => {
+  return (
+    <div className="class_modal">
+      <img
+        src={iconoClose}
+        alt="icono cerrar"
+        className="class_closed"
+        onClick={() => closeModal(false)}
+      />
+
+      <h2>Rick and Mortyyyy!</h2>
+    </div>
+  );
+};
+
+export default ModalInfo;
